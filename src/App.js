1d5ccde0a933bbe768aed01c2573ab0d7378dc95
ACHARYA, CHANDAN <chandan.acharya@ge.com>
2022-02-19 14:18:17 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        How are you
+      </header>
+    </div>
+  );
+}
+
+export default App;
